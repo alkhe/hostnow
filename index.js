6e15130265b7595166fa1ee7c8afbecf4f136218
edge <dev@ionized.io>
2016-05-09 01:50:33 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let express = require('express'),
+	path = require('path'),
+	fs = require('fs'),
+	app = express();
+
+let args = process.argv.slice(2),
+	root = path.resolve(args[0] || process.cwd()),
+	port = Number(args[1] || 80);
+
+app.use(express.static(root));
+
+app.get('*', (req, res) => {
+	let local = req.path,
+		absolute = path.join(root, local),
+		children = ['.', '..'].concat(fs.readdirSync(absolute)),
+		links = children.map(c => '<a href="/' + c + '">' + c + '</a>').join('<br />');
+
+	res.send(links);
+});
+
+app.listen(port, err => {
+	if (err) {
+		console.log('Error', err);
+		process.exit(1);
+	}
+	console.log('Serving ' + root + ' on port ' + port + '.');
+});
