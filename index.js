5b42555eba6797748970328c0da79a9be5d34a35
edge <dev@ionized.io>
2016-05-09 02:08:23 -0400

    fix nested child misses

--- index.js
+++ index.js
@@ -9,13 +9,24 @@ let args = process.argv.slice(2),
 	root = path.resolve(args[0] || process.cwd()),
 	port = Number(args[1] || 80);
 
-app.use(express.static(root));
+app.use((req, res, next) => {
+	req.path = unescape(req.path);
+	next();
+});
+
+app.use(express.static(root, {
+	dotfiles: 'allow'
+}));
 
 app.get('*', (req, res) => {
 	let local = req.path,
 		absolute = path.join(root, local),
-		children = ['.', '..'].concat(fs.readdirSync(absolute)),
-		links = children.map(c => '<a href="/' + c + '">' + c + '</a>').join('<br />');
+		children = ['.', '..'].concat(fs.readdirSync(absolute));
+
+	let childToHtml = c =>
+		`<a href='${ path.join(local, c) }'>${ c }</a>`;
+
+	let links = children.map(childToHtml).join('<br />');
 
 	res.send(links);
 });
